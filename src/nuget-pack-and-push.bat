86e63b7637fdde43ecc35e8215bf127d9525a896
IvanBoyko <boiko.ivan@gmail.com>
2017-01-12 11:41:08 +0000

    nuget-pack-and-push.bat - commented out "nuget push" as it should be done carefully

--- src/nuget-pack-and-push.bat
+++ src/nuget-pack-and-push.bat
@@ -2,10 +2,8 @@
 set NUGET_SOURCE=https://api.nuget.org/v3/index.json
 
 nuget pack WixAutoHarvest.ConsoleApp.nuspec
-pause
-
 nuget pack WixAutoHarvest.WebApp.nuspec
 pause
 
-nuget push *.nupkg -Source %NUGET_SOURCE%
-pause
+REM nuget push *.nupkg -Source %NUGET_SOURCE%
+REM pause
